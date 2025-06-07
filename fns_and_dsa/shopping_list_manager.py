23f68f0b528f89780175068820ce65b2cc49aa05
Victoria <toriamwendeo11@gmail.com>
2025-06-07 21:35:10 +0300

    Add shopping_list_manager.py

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,8 +1,8 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
-    print("3. View list")
+    print("3. View List")
     print("4. Exit")
 
 def main():
